b2866ed6613723f1ec70948b812308b85ea5d36e
hooloong <hooloong@vip.qq.com>
2018-03-31 19:49:59 +0800

    add 001 examples.

--- freshman01/taobao_finduser.py
+++ freshman01/taobao_finduser.py
@@ -10,7 +10,7 @@ goods_data_file = "../../data/fresh_comp_offline/tianchi_fresh_comp_train_item.c
 
 user_data = pd.read_csv(user_data_file, header=0)
 goods_data = pd.read_csv(goods_data_file, header=0)
-goods_data.to_csv("./items.csv",index=False,index_label=False, columns=["item_geohash"],)
+goods_data.to_csv("./items.csv",index=False,index_label=False, columns=["item_id"],)
 # train_goods = scale(np.asarray(goods_data.ix[:, 2]))
 # train_user_data = scale(np.asarray(goods_data.ix[:, 2]))
 # print(train_user_data)
