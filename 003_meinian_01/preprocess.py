e56210d80cf0b7beae6cdecea325299ece586456
hooloong <hooloongge@sigmadesigns.com>
2018-04-09 19:36:27 +0800

    003: preprocess

---
+++ 003_meinian_01/preprocess.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    data_part1 = pd.read_csv('../../data/aliyun_meinian/meinian_round1_data_part1_20180408.csv', sep='$')
+
+    data_part2 = pd.read_csv('../../data/aliyun_meinian/meinian_round1_data_part2_20180408.csv', sep='$')
+
+
